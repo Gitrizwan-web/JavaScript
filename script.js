088b5c17be20118f30090962b109148209652802
Rizwan Fahim <119812012+Rizwanfahim@users.noreply.github.com>
2023-01-25 22:44:22 +0500

    chapter alart

---
+++ script.js
@@ -0,0 +1,16 @@
+// ***Chapter 1***
+// Question no 1
+// alert("Welcome to our website!");
+
+// Question 2
+// alert("Error! Please enter a valid password.")
+// Question 3
+// alert("Welcome to js Land..\n Happy Coding")
+// Question 4
+        // alert("Welcome to js Land.. \n prevent this page from creating additional dailogs.")
+// Question 5
+//    alert("Hello...I can run js through my web browser's console")
+// Question 6 & 7
+// document.getElementById("alert-button").addEventListener("click", function(){
+//     alert("Button clicked!");
+//   });
