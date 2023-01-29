163f58b1c0d8ff08db3c81159ecf0df24b4deeb8
Rizwan Fahim <119812012+Rizwanfahim@users.noreply.github.com>
2023-01-29 10:25:52 +0500

    js assigment

--- script.js
+++ script.js
@@ -7,10 +7,81 @@
 // Question 3
 // alert("Welcome to js Land..\n Happy Coding")
 // Question 4
-        // alert("Welcome to js Land.. \n prevent this page from creating additional dailogs.")
+// alert("Welcome to js Land.. \n prevent this page from creating additional dailogs.")
 // Question 5
 //    alert("Hello...I can run js through my web browser's console")
 // Question 6 & 7
 // document.getElementById("alert-button").addEventListener("click", function(){
 //     alert("Button clicked!");
 //   });
+// **** chapterTwo****
+// Question1
+// var name = "Rizwan"
+// alert(name)
+// Question2
+// var name= "Rizwan "
+// var fullName="Fahim"
+// var add = name + fullName
+// alert (add)
+// Question3
+//  var massage = "Hello world"
+// alert(massage)
+// Question 4
+// var name = "Rizwan Fahim"
+// var age = " 18 year's old "
+// var skill = "Web development"
+// alert (name)
+// alert(age)
+// alert (skill)
+// Question 5
+// var p1 = "pizza\n"
+// var p2 = "pizz\n"
+// var p3 = "pi\n"
+// var p4 = "p\n"
+// var total = p1+p2+p3+p4
+// alert(total)
+// Question6
+// var email="My email adress in rizwanfahim004@gmail.com"
+// alert(email)
+// Question7
+// var book = "A smarter way to learn JavaScript"
+// alert(book)
+// Question no 8
+// document.write("<h1>Yah'I can write HTML content through Javascript<h1/>")
+// Questionno9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// ***chapter3***
+// Question1
+// var age = 18
+// var a = "I am "
+// var a2 = " year's old"
+// var a3 = a +age +a2
+// alert(a3)
+
+// Question 3
+// var qoute = "You have visited this site N times."
+// alert(qoute)
+// Question4
+// document.write("<h1>My Birth year is 1990<h1/> <h2>Data type of my declared Varaible is number ")
+// Question 5
+// document.write ("John Doe ordered 5 T-shirt(s) on XYZ Clothing store.")
+// ****chapter4***
+// Question1
+// var variable1 = "Hello, World!";
+// var variable2 = "Testing...";
+// var variable3 = 42;
+// Question2
+// 1
+// var x;
+// 2
+// console.log(x); 
+// var x = 3;
+// question 3
+//    var n1 = "Number", n2="$",n3="_.",n4 = "letter " , n5=  5  ,n6= "Sensitive". n7="Keyword."
+//    document.write("<h1>Rules for naming JS variables<h1/>")
+//    document.write("Variable names can only contain.",n1 ," and ",n2," and ",n3,"</br>")
+//    document.write("Variables must begin with a ",n4," , ",n5,"or",n3 ," For example $name, _name or name </br>") 
+//    document.write ("Variable names are case ",n6,"<br/>")
+//    document.write("Variable names should not be JS",n6)
+
+
